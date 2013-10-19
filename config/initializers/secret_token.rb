35f528551922da4afcab6c2426af5e0b40dcaa25
Thibaut Grange <thibaut@kabest.me>
2013-10-19 20:55:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kabestnum::Application.config.secret_key_base = 'd62d0df491d5577b1cfd68f8246fdd74dcdcec2374bd37c40a75f3ddaa836341eecd5ccbb11d06c5587f54e72ddcc857ac41fb8476718499068fc65a2a9cc563'
